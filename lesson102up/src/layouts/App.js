13a65689f1abed25452028cf4a0f141f96be7063
Hubcio <hubert.firek@o2.pl>
2021-02-21 07:57:08 +0100

    up

--- lesson102up/src/layouts/App.js
+++ lesson102up/src/layouts/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   state = {};
   render() {
     return (
-      <BrowserRouter basename={ProcessingInstruction.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="App">
           <header>{<Header />}</header>
           <main>
